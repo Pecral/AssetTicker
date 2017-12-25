a107b7388adbc997da72713284e3f5ce8b63bc94
Pecral <pecral.dev@gmail.com>
2017-12-25 20:15:34 +0100

    added custom exchange name to enum resolver to ignore casing

--- src/app/shared/models/exchange-ticker-type.ts
+++ src/app/shared/models/exchange-ticker-type.ts
@@ -2,3 +2,18 @@ export enum ExchangeTickerType {
     Bitfinex,
     GDAX
 }
+
+export class ExchangeNameResolver {
+   static Resolve(name: string): ExchangeTickerType {
+      let nameNoCase = name.toLowerCase();
+      switch(nameNoCase) {
+         case "bitfinex":
+            return ExchangeTickerType.Bitfinex;
+
+         case "gdax":
+         return ExchangeTickerType.GDAX;
+      }
+
+      return;
+   }
+}
